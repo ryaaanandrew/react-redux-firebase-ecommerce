b03adaedc3e04c8f68131b22c8c7256abeb05919
Ryan Andrew Sibat <ryan@Ryans-MacBook-Pro.local>
2020-08-05 12:15:29 -0400

    added header w/ base styling

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = () => {
+    return(
+        <header className="myHeader">
+            my Header
+        </header>
+    );
+};
+
+export default Header;
