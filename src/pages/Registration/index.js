e87c56abc8ad13d6f442d10a45ea9c72d0dee2f5
Ryan Andrew Sibat <ryan@Ryans-MacBook-Pro.local>
2020-08-06 13:19:35 -0400

    react router set up

---
+++ src/pages/Registration/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class Registration extends Component {
+    render() {
+        return(
+            <h1>Registration</h1>
+        );
+    };
+};
+
+export default Registration;
